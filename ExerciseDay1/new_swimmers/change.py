3cf3bf418741c0c0b44377a9e4986069de930e67
manulera <manulera14@gmail.com>
2017-09-17 15:58:39 +0200

    Added comments to the python scripts

--- ExerciseDay1/new_swimmers/change.py
+++ ExerciseDay1/new_swimmers/change.py
@@ -1,6 +1,6 @@
 
-with open('swimmers.csv','r') as ins:
+with open('swimmers_female.csv','r') as ins:
     for line in ins:
-        l = line.split(';')
-        print(';'.join(l[:-1])+';'+l[-1].split()[-1] )
+        l = line.split(',')
+        print(','.join(l[:-1])+','+l[-1].split()[-1])
         
